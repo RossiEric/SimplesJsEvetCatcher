abcb5542869f9fda22aa5205f2a2880ba4b5e4a8
Eric Rossi <eric.r.rossi@gmail.com>
2021-02-28 10:06:56 -0500

    module, set event label
    putting in module structure.
    function for set event label based on target element.

--- JeventCatcher/index.js
+++ JeventCatcher/index.js
@@ -1,53 +1,68 @@
-
-
-
-function ready(fn) {
-    if (document.readyState != 'loading') {
-        fn();
-    } else {
-        document.addEventListener('DOMContentLoaded', fn);
-    }
-}
-
-function Init() {
-    GaListner();
-}
-
-function GaListner() {
-
-    console.log('ready');
-
+var JeventCatcher = (function () {
     
-    //document.querySelectorAll('*').forEach(item => {
-    document.querySelectorAll('.GaListener').forEach(item => {
-        item.addEventListener('click', e => {
-            console.log(e);
+    function ready(fn) {
+        if (document.readyState != 'loading') {
+            fn();
+        } else {
+            document.addEventListener('DOMContentLoaded', fn);
+        }
+    }
+    
+    //exec init on ready
+    ready(Init);
 
-            console.log('send', 'event', {
-                eventCategory: e.currentTarget.localName,
-                eventAction: e.type,
-                eventLabel: e.target.innerText,
-                //transport: 'beacon' //para url externas
-                //nonInteraction: true
+    function Init() {
+        GaListner();
+    }
+    
+    function GaListner() {
+    
+        console.log('ready');
+
+        //document.querySelectorAll('*').forEach(item => {
+        document.querySelectorAll('.GaListener').forEach(item => {
+            item.addEventListener('click', e => {
+                console.log(e);
+    
+                console.log('send', 'event', {
+                    eventCategory: e.currentTarget.localName,
+                    eventAction: e.type,
+                    eventLabel: SetEventLabel(e.target)
+                    //transport: 'beacon' //para url externas
+                    //nonInteraction: true
+                });
+    
             });
-
+    
+            item.addEventListener('submit', e => {
+                console.log(e);
+            });
+    
+            //beforeprint
+            //change
+            //dblclick
+            //ended
+            //scroll //wheel
+            //touchstart
+    
+    
+    
         });
+    }
 
-        item.addEventListener('submit', e => {
-            console.log(e);
-        });
+    function SetEventLabel(target) {
+        if (target.id != null) {
+            return target.id;
+        }else if (target.name != null) {
+            return target.name;
+        }else if (target.value != null) {
+            return target.value;
+        }
+    }
 
-        //beforeprint
-        //change
-        //dblclick
-        //ended
-        //scroll //wheel
-        //touchstart
+    return {
+        Init:Init
+    };
+}());
 
 
-
-    });
-}
-
-//exec init on ready
-ready(Init);
